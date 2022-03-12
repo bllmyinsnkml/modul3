ff6f2a4172a0c2c13b149c59a43a3d47c1fb223b
unknown <bellamyinsank@student.telkomuniversity.ac.id>
2022-03-12 11:13:22 +0700

    menambahkan class KodeBuah

---
+++ modul3_1302200086/KodeBuah.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul3_1302200086
+{
+    public class KodeBuah
+    { 
+        public enum NamaBuah {
+            Apel,
+            Aprikot,
+            Alpukat,
+            Pisang,
+            Paprika,
+            Blackberry,
+            Ceri,
+            Kelapa,
+            Jagung,
+            Kurma,
+            Durian,
+            Anggur,
+            Melon,
+            Semangka
+        };
+
+        public KodeBuah()
+        {
+        }
+        public string GetKodeBuah(NamaBuah inputBuah)
+        {
+            string[] arrayKodeBuah = {"A00", "B00", "C00", "D00", "E00", "F00", "H00", "I00", "J00", "K00", "L00", "M00", "N00", "O00"};
+            int PanjangArray = arrayKodeBuah.Length;
+
+            return arrayKodeBuah[(int)inputBuah];
+        }
+    }
+}
--- modul3_1302200086/Program.cs
+++ modul3_1302200086/Program.cs
@@ -1,2 +1,61 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using modul3_1302200086;
+using static modul3_1302200086.KodeBuah;
+
+KodeBuah inputBuah = new KodeBuah();
+
+Console.Write(KodeBuah.NamaBuah.Apel);
+Console.Write(" ");
+Console.WriteLine(inputBuah.GetKodeBuah(KodeBuah.NamaBuah.Apel));
+
+Console.Write(KodeBuah.NamaBuah.Aprikot);
+Console.Write(" ");
+Console.WriteLine(inputBuah.GetKodeBuah(KodeBuah.NamaBuah.Aprikot));
+
+Console.Write(KodeBuah.NamaBuah.Alpukat);
+Console.Write(" ");
+Console.WriteLine(inputBuah.GetKodeBuah(KodeBuah.NamaBuah.Alpukat));
+
+Console.Write(KodeBuah.NamaBuah.Pisang);
+Console.Write(" ");
+Console.WriteLine(inputBuah.GetKodeBuah(KodeBuah.NamaBuah.Pisang));
+
+Console.Write(KodeBuah.NamaBuah.Paprika);
+Console.Write(" ");
+Console.WriteLine(inputBuah.GetKodeBuah(KodeBuah.NamaBuah.Paprika));
+
+Console.Write(KodeBuah.NamaBuah.Blackberry);
+Console.Write(" ");
+Console.WriteLine(inputBuah.GetKodeBuah(KodeBuah.NamaBuah.Blackberry));
+
+Console.Write(KodeBuah.NamaBuah.Ceri);
+Console.Write(" ");
+Console.WriteLine(inputBuah.GetKodeBuah(KodeBuah.NamaBuah.Ceri));
+
+Console.Write(KodeBuah.NamaBuah.Kelapa);
+Console.Write(" ");
+Console.WriteLine(inputBuah.GetKodeBuah(KodeBuah.NamaBuah.Kelapa));
+
+Console.Write(KodeBuah.NamaBuah.Jagung);
+Console.Write(" ");
+Console.WriteLine(inputBuah.GetKodeBuah(KodeBuah.NamaBuah.Jagung));
+
+Console.Write(KodeBuah.NamaBuah.Kurma);
+Console.Write(" ");
+Console.WriteLine(inputBuah.GetKodeBuah(KodeBuah.NamaBuah.Kurma));
+
+Console.Write(KodeBuah.NamaBuah.Durian);
+Console.Write(" ");
+Console.WriteLine(inputBuah.GetKodeBuah(KodeBuah.NamaBuah.Durian));
+
+Console.Write(KodeBuah.NamaBuah.Anggur);
+Console.Write(" ");
+Console.WriteLine(inputBuah.GetKodeBuah(KodeBuah.NamaBuah.Anggur));
+
+Console.Write(KodeBuah.NamaBuah.Melon);
+Console.Write(" ");
+Console.WriteLine(inputBuah.GetKodeBuah(KodeBuah.NamaBuah.Melon));
+
+Console.Write(KodeBuah.NamaBuah.Semangka);
+Console.Write(" ");
+Console.WriteLine(inputBuah.GetKodeBuah(KodeBuah.NamaBuah.Semangka));
